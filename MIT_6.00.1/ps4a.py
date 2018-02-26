4c6fe9be55c1c49e1f42bf8522a5379ac165d73a
Peter <dchen@albany.edu>
2018-02-26 17:45:15 -0500

    Completed wordgame version a

--- MIT_6.00.1/ps4a.py
+++ MIT_6.00.1/ps4a.py
@@ -317,10 +317,23 @@ def playGame(wordList):
     2) When done playing the hand, repeat from step 1    
     """
     # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-   
-
-
+    user_input = ""
+    current_hand = {}
+    
+    while user_input != 'e':
+        user_input = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if user_input == 'e':
+            return None 
+        elif user_input == 'n':
+            current_hand = dealHand(HAND_SIZE).copy()
+            playHand(current_hand, wordList, HAND_SIZE)
+        elif user_input == 'r':
+            if current_hand == {}:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(current_hand, wordList, HAND_SIZE)
+        else:
+            print("Invalid command.")
 
 #
 # Build data structures used for entire session and play game
